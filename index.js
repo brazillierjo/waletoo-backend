3bb3a99d72b78af5519f6221105532f264e6a4db
brazillierjo <j.brazillier@gmail.com>
2022-08-17 21:54:17 +0200

    initialize backend

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port)
+})
+
