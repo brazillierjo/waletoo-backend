cd74aec7e7c1006046aabad230f37138f8070767
brazillierjo <j.brazillier@gmail.com>
2022-10-27 23:58:52 +0200

    [++][ADD][CORS] add cors options

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const userRoutes = require("./routes/auth.routes");
 const incomesRoutes = require("./routes/incomes.routes");
 const expensesRoutes = require("./routes/expenses.routes");
 const { verifyToken } = require("./middlewares/auth.middleware");
+const cors = require("cors");
 require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(
