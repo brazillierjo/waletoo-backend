35c420b2bd5a79d1200c30e2a5e643762f7f4db3
brazillierjo <j.brazillier@gmail.com>
2022-12-24 01:26:01 +0100

    allow cors on new port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   credentials: true,
   optionSuccessStatus: 200
 };
